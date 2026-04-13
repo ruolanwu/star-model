eca5c8e613f6f7a9d1c66a652ee300cedf6b1612
Ruolan Wu <wruolan109@gmail.com>
2026-04-12 22:08:22 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let model3D;
+let angleX = 0;
+let angleY = 0;
+
+function preload() {
+  model3D = loadModel("star.obj", true);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+}
+
+function draw() {
+  background(0);
+  ambientLight(120);
+  directionalLight(255, 255, 255, 0.5, 1, -0.5);
+  rotateX(angleX);
+  rotateY(angleY);
+  scale(1);
+  model(model3D);
+}
+
+function mouseDragged() {
+  angleY += movedX * 0.01;
+  angleX += movedY * 0.01;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
